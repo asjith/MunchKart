c394115fe492e7d247b7b0e3360b6da42191a29f
anusreesjith <anusreesjith@gmail.com>
2023-10-10 22:48:32 +0530

    episode 2

--- App.js
+++ App.js
@@ -1,15 +1,10 @@
-// <div id="parent">
-//     <div id="child">
-//         <h1>h1 tag</h1>
-//         <h2>h2 tag</h2>
-//     </div>
-// </div>  
-
-
-const heading1 = React.createElement("h1", {id:"heading1"}, "h1 tag");
+import React from "react";
+import ReactDOM from "react-dom/client";
+const heading1 = React.createElement("h1", { id: "heading1" }, "h1 tag");
 const heading2 = React.createElement("h2", {}, "h2 tag");
 const divChild = React.createElement("div", { id: "child" }, [heading1, heading2]);
 const divParent = React.createElement("div", { id: "parent" }, divChild);
+
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
