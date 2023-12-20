0adc06605d8342920e53aea2f26c102689dc40bb
anusreesjith <anusreesjith@gmail.com>
2023-12-20 07:25:09 +0530

    episode 3

--- App.js
+++ App.js
@@ -1,11 +1,42 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-const heading1 = React.createElement("h1", { id: "heading1" }, "h1 tag");
-const heading2 = React.createElement("h2", {}, "h2 tag");
-const divChild = React.createElement("div", { id: "child" }, [heading1, heading2]);
-const divParent = React.createElement("div", { id: "parent" }, divChild);
 
+const element = <h2>This is an element</h2>;
 
+const TitleComponent = () => {
+    return (
+        <div>
+            <h1>Namaste React</h1>
+            {element}                 
+        </div>
+        
+    );
+};
+
+const HeaderComponent = () => {
+    return (
+        <div>
+            <h1>Welcome to Namaste React</h1>
+            <TitleComponent />
+            <TitleComponent></TitleComponent>
+            {TitleComponent()}
+        </div>
+    );
+};
+/*
+the HeaderComponent written is an arrow function. The same can be written also as:
+const HeaderComponent = function () {
+    return (
+        <div>
+            <TitleComponent />
+            <h1>Welcome to Namaste React</h1>
+        </div>
+    );
+};
+
+*/
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(divParent);
+//root.render(jsxElement);
+root.render(<HeaderComponent/>);
+
