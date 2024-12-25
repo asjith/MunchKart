4a9610a582b2aa534e7e8d8d6c800afe340e4865
anusreesjith <anusreesjith@gmail.com>
2024-12-25 12:02:53 +0530

    episode 7

--- src/App.js
+++ src/App.js
@@ -2,18 +2,48 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./component/Header";
 import Body from "./component/Body";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router";
+import About from "./component/About";
+import Contact from "./component/Contact";
+import Error from "./component/Error";
+import RestaurantMenu from "./component/RestaurantMenu";
 
 const AppLayout = () => {
-    return (
-            <div className="app">
-                    <Header />
-                    <Body />
-                    
-            </div>
-    )
-}
+  return (
+    <div className="app">
+      <Header />
+      <Outlet />
+    </div>
+  );
+};
+
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <AppLayout />,
+    children: [
+      {
+        path: "/about",
+        element: <About />,
+      },
+      {
+        path: "/contact",
+        element: <Contact />,
+      },
+      {
+        path: "/",
+        element: <Body />,
+      },
+      {
+        path: "/restaurants/:resId",
+        element: <RestaurantMenu />,
+      },
+    ],
+    errorElement: <Error />,
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 //root.render(jsxElement);
-root.render(<AppLayout/>);
-
+root.render(<RouterProvider router={appRouter} />);
+//root.render(<AppLayout />);
---
+++ src/component/RestaurantMenu.js
@@ -0,0 +1,56 @@
+import { useState, useEffect } from "react";
+import Shimmer from "./Shimmer";
+import { useParams } from "react-router";
+import { MENU_API } from "../utils/constants";
+
+const RestaurantMenu = () => {
+  const [resInfo, setResInfo] = useState(null);
+
+  const { resId } = useParams();
+
+  useEffect(() => {
+    fetchMenu();
+  }, []);
+
+  const fetchMenu = async () => {
+    const data = await fetch(MENU_API + resId);
+    const json = await data.json();
+    console.log(json);
+    setResInfo(json.data);
+  };
+
+  if (resInfo === null) return <Shimmer />;
+  else {
+    const { name, cuisines, costForTwoMessage } =
+      resInfo?.cards[2]?.card?.card?.info;
+    const { itemCards } =
+      resInfo?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards[2]?.card
+        ?.card;
+    console.log(itemCards);
+
+    return (
+      <div className="menu">
+        <h1>{name}</h1>
+        <p>
+          {cuisines} - {costForTwoMessage}
+        </p>
+        <h2>Menu</h2>
+        <ul>
+          {itemCards.map((item) => (
+            <li key={item?.card?.info?.id}>
+              {item?.card?.info?.name} - Rs.
+              {item?.card?.info?.price / 100 ||
+                item?.card?.info?.defaultPrice / 100}
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+};
+
+export default RestaurantMenu;
+
+//https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.960059122809971&lng=77.57337538383284&restaurantId=2415&catalog_qa=undefined&submitAction=ENTER
+//https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.89960&lng=80.22090&restaurantId=10892&catalog_qa=undefined&submitAction=ENTER -> pizza hut menu api
+//restaurantId = 43836 or 1452
